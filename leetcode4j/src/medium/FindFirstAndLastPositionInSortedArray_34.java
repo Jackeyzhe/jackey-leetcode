e6cf8d0bf3bb7e7a9a070e40cd607e651e0bfe55
jackeyzhe <jackeyzhe59@gmail.com>
2025-01-21 22:57:04 +0800

    resolve 34

---
+++ leetcode4j/src/medium/FindFirstAndLastPositionInSortedArray_34.java
@@ -0,0 +1,39 @@
+package medium;
+
+import java.util.Arrays;
+
+public class FindFirstAndLastPositionInSortedArray_34 {
+
+    public static void main(String[] args) {
+        FindFirstAndLastPositionInSortedArray_34 solution = new FindFirstAndLastPositionInSortedArray_34();
+        int[] nums = new int[] {5,7,7,8,8,10};
+        int target = 8;
+        int[] result = solution.searchRange(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int leftIndex = binarySearch(nums, target, true);
+        int rightIndex = binarySearch(nums, target, false) - 1;
+        if (leftIndex <= rightIndex && rightIndex <= nums.length && nums[leftIndex] == target && nums[rightIndex] == target) {
+            return new int[] {leftIndex, rightIndex};
+        }
+        return new int[] {-1, -1};
+    }
+
+    private int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
