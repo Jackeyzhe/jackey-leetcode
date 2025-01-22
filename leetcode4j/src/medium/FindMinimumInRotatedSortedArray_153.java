966bf793d6ad5f524fc1fb267acffbc326b88c53
jackeyzhe <jackeyzhe59@gmail.com>
2025-01-22 22:52:43 +0800

    resolve 153

---
+++ leetcode4j/src/medium/FindMinimumInRotatedSortedArray_153.java
@@ -0,0 +1,25 @@
+package medium;
+
+public class FindMinimumInRotatedSortedArray_153 {
+
+    public static void main(String[] args) {
+        FindMinimumInRotatedSortedArray_153 solution = new FindMinimumInRotatedSortedArray_153();
+        int[] nums = new int[]{3,4,5,1,2};
+        int result = solution.findMin(nums);
+        System.out.println(result);
+    }
+
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+}
