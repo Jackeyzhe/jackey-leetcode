4679c052239e9b10426ae082697bd63738af10d4
jackeyzhe <jackeyzhe59@gmail.com>
2025-05-04 09:48:23 +0800

    resolve 189

---
+++ leetcode4j/src/medium/RotateArray_189.java
@@ -0,0 +1,31 @@
+package medium;
+
+import java.util.Arrays;
+
+public class RotateArray_189 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        RotateArray_189 solution = new RotateArray_189();
+        solution.rotate(nums, k);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
