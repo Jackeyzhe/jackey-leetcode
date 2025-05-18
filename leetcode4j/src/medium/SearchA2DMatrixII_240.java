a913ad6d113a01bba30286605050240bcaf15f63
Jackeyzhe <jackeyzhe59@163.com>
2025-05-18 18:13:34 +0800

    resolve 240

---
+++ leetcode4j/src/medium/SearchA2DMatrixII_240.java
@@ -0,0 +1,29 @@
+package medium;
+
+public class SearchA2DMatrixII_240 {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 5;
+        SearchA2DMatrixII_240 solution = new SearchA2DMatrixII_240();
+        System.out.println(solution.searchMatrix(matrix, target));
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int x = 0;
+        int y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] > target) {
+                y--;
+            } else {
+                x++;
+            }
+        }
+        return false;
+    }
+}
