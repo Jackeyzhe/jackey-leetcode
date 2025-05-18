8434e44ef9ecd992f1dbd582fe03c400455e43b0
Jackeyzhe <jackeyzhe59@163.com>
2025-05-18 18:01:03 +0800

    resolve 48

---
+++ leetcode4j/src/medium/RotateImage_48.java
@@ -0,0 +1,32 @@
+package medium;
+
+import java.util.Arrays;
+
+public class RotateImage_48 {
+
+    public static void main(String[] args) {
+        int[][] matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        RotateImage_48 solution = new RotateImage_48();
+        solution.rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+}
