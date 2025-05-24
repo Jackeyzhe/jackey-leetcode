5766cf8cc42bdf8bc880a6b511d2722f8df17571
Jackeyzhe <jackeyzhe59@163.com>
2025-05-24 14:07:44 +0800

    resolve 437

---
+++ leetcode4j/src/medium/PathSum_437.java
@@ -0,0 +1,40 @@
+package medium;
+
+import common.tree.TreeNode;
+
+public class PathSum_437 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(10, new TreeNode(5, new TreeNode(3, new TreeNode(3), new TreeNode(-2)), new TreeNode(2, null, new TreeNode(1))), new TreeNode(-3, null, new TreeNode(11)));
+        PathSum_437 solution = new PathSum_437();
+        int targetSum = 8;
+        int result = solution.pathSum(root, targetSum);
+        System.out.println(result);
+    }
+
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int ret = rootSum(root, targetSum);
+        ret += pathSum(root.left, targetSum);
+        ret += pathSum(root.right, targetSum);
+        return ret;
+    }
+
+    private int rootSum(TreeNode root, int targetSum) {
+        int ret = 0;
+        if (root == null) {
+            return 0;
+        }
+
+        int val = root.val;
+        if (val == targetSum) {
+           ret++;
+        }
+        ret += rootSum(root.left, targetSum - val);
+        ret += rootSum(root.right, targetSum - val);
+        return ret;
+    }
+}
