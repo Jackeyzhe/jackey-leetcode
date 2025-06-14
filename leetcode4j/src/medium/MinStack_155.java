8c6ee3da4cd0382b96ec872ed8495cbb08c564ac
jackeyzhe <jackeyzhe59@gmail.com>
2025-06-14 22:30:41 +0800

    resolve 155

---
+++ leetcode4j/src/medium/MinStack_155.java
@@ -0,0 +1,49 @@
+package medium;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class MinStack_155 {
+
+    public static void main(String[] args) {
+        // "MinStack","push","push","push","getMin","pop","top","getMin"
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+}
+
+class MinStack {
+
+    Deque<Integer> xStack;
+    Deque<Integer> minStack;
+
+    public MinStack() {
+        xStack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int val) {
+        xStack.push(val);
+        minStack.push(Math.min(minStack.peek(), val));
+    }
+
+    public void pop() {
+        xStack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return xStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
