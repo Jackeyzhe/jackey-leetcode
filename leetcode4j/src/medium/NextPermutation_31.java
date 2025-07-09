944089cc0b5027d08809e7d7afc5ed3946a0b951
jackeyzhe <jackeyzhe59@gmail.com>
2025-07-10 00:11:56 +0800

    resolve 31

---
+++ leetcode4j/src/medium/NextPermutation_31.java
@@ -0,0 +1,44 @@
+package medium;
+
+import java.util.Arrays;
+
+public class NextPermutation_31 {
+
+    public static void main(String[] args) {
+        NextPermutation_31 solution = new NextPermutation_31();
+        int[] nums = {1, 2, 3};
+        solution.nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start) {
+        int left = start;
+        int right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
